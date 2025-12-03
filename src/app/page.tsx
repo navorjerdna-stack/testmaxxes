162f6d799af63cff5a9bd3224924f39e04970d0b
navorjerdna-stack <navor.jerdna@gmail.com>
2025-12-03 12:37:59 +0100

    minimal working page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <h1 style={{fontSize:"3rem",textAlign:"center",marginTop:"40vh",color:"#ec4899"}}>
-      ŽIVA JE – DELUJE 100 %!!! 🎉
-    </h1>
-  );
+  return <h1 style={{fontSize:"5rem",textAlign:"center",marginTop:"40vh",color:"#ec4899"}}>ŽIVA JE – DELUJE!!! 🎉</h1>;
 }
