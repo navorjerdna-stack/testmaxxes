1c4a0a94b883233e918f0a8279a141f45b888d73
navorjerdna-stack <navor.jerdna@gmail.com>
2025-12-03 13:13:33 +0100

    Refactor h1 element for improved readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <h1 style={{fontSize:"5rem",textAlign:"center",marginTop:"40vh",color:"#ec4899"}}>
-      ŽIVA JE – DELUJE!!! 🎉
+    <h1
+      style={{
+        fontSize: "5rem",
+        textAlign: "center",
+        marginTop: "40vh",
+        color: "#ec4899",
+      }}
+    >
+      ŽIVA JE – DELUJE!!!
     </h1>
   );
 }
